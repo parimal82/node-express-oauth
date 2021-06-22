ad77010e6e043caa06625c4fe07983c11c1cd2f3
tmq8flh <phalder@ups.com>
2021-06-23 00:29:19 +0530

    Completed Authorization Server

--- authorization-server.js
+++ authorization-server.js
@@ -96,7 +96,7 @@ app.post("/approve", (req, res) => {
 		code,
 		state: clientReq.state,
 	}
-	res.redirect(url.format(redirectUri))
+	res.status(302).redirect(url.format(redirectUri))
 })
 
 app.post("/token", (req, res) => {
